2386b243cce3f4ef26a406fbe98ea8683468f498
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-11-26 23:40:37 -0500

    Add approval tests over Gilded Rose kata

--- gilded-rose.test.js
+++ gilded-rose.test.js
@@ -1,9 +1,22 @@
 import { Item, Shop } from "./gilded-rose";
 
 describe("Gilded Rose", () => {
-  it("should foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+  it("should update quality", () => {
+    expect(doUpdateQuality).toVerifyAllCombinations(
+      [
+        "foo",
+        "Aged Brie",
+        "Backstage passes to a TAFKAL80ETC concert",
+        "Sulfuras, Hand of Ragnaros"
+      ],
+      [-1, 0, 2, 6, 11],
+      [0, 1, 49, 50]
+    );
   });
+
+  function doUpdateQuality(name, sellIn, quality) {
+    const gildedRose = new Shop([new Item(name, sellIn, quality)]);
+    const items = gildedRose.updateQuality();
+    return items[0];
+  }
 });
